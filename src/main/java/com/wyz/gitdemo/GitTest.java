38e856695bb7d5114fdb8c5c6d4fd84430698ac6
wyz <2750565592@qq.com>
2021-12-02 14:43:26 +0800

    second commit

--- src/main/java/com/wyz/gitdemo/GitTest.java
+++ src/main/java/com/wyz/gitdemo/GitTest.java
@@ -3,5 +3,6 @@ package com.wyz.gitdemo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("test git");
+        System.out.println("test git2");
     }
 }
